690b1e183586fb7205dc6129dd3f019a230ae1cf
KirbyXstar <kirbyXstar@gmail.com>
2020-11-30 03:11:17 -0600

    main file and commands (Not finished)

---
+++ main.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const customconfig = require('./config.discord.json');
+const mongoose = require('mongoose');
+const ms = require('ms')
